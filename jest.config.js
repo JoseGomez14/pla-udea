d2da8a841643b2fc9aead6f5e10b3540a6bd13b9
desarrollo.ingenieria19 <johndoe@example.com>
2023-05-15 12:20:23 -0500

    Pruebas a componentes y páginas

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
+  //testEnvironment: 'jest-environment-jsdom-fourteen',
   testEnvironment: 'jsdom',
   roots: ['<rootDir>/src'],
   transform: {
